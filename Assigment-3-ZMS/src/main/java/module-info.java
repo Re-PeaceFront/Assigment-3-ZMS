ab7b46667b649b58fb4e1e5c5ad8371c7f353348
Gasore Rene Kazuba <167144827+Re-PeaceFront@users.noreply.github.com>
2025-10-31 18:08:06 -0400

    Project Start
    Intel J project setup with JAVAFX

---
+++ Assigment-3-ZMS/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assigment3zms {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assigment3zms to javafx.fxml;
+    exports com.example.assigment3zms;
+}
