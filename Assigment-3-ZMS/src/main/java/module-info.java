87342de6a02639bb3820d454ea0eb5a7989eacf1
MrHarte <111158364+MrHarte@users.noreply.github.com>
2025-11-01 12:12:58 -0400

    Added Model folder to be included in builds

--- Assigment-3-ZMS/src/main/java/module-info.java
+++ Assigment-3-ZMS/src/main/java/module-info.java
@@ -7,4 +7,6 @@ module com.example.assigment3zms {
     exports com.example.assigment3zms;
     exports com.example.assigment3zms.Controllers;
     opens com.example.assigment3zms.Controllers to javafx.fxml;
+    exports com.example.assigment3zms.Model;
+    opens com.example.assigment3zms.Model to javafx.fxml;
 }
