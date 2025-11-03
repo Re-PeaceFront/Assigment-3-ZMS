b7a113b3f6801660d0c3d1b3469047137dc47ea6
MrHarte <111158364+MrHarte@users.noreply.github.com>
2025-11-03 13:21:10 -0500

    Added necessary controller elements

--- Assigment-3-ZMS/src/main/java/com/example/assigment3zms/Controllers/AnimalViewController.java
+++ Assigment-3-ZMS/src/main/java/com/example/assigment3zms/Controllers/AnimalViewController.java
@@ -1,4 +1,27 @@
 package com.example.assigment3zms.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
 public class AnimalViewController {
+    @FXML
+    private ChoiceBox<String> aTypeChoiceBox;
+
+    @FXML
+    private Spinner<Double> aAgeSpinner;
+
+    @FXML
+    private TextField aNameTextField;
+
+    @FXML
+    protected void onSaveButtonClick() {
+
+    }
+
+    @FXML
+    protected void onBackButtonClick() {
+
+    }
 }
