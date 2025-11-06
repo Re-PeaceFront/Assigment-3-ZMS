6da98c0fcec247350bf87fe489a668f94f91c4cb
Gasore Rene Kazuba <167144827+Re-PeaceFront@users.noreply.github.com>
2025-11-06 15:23:21 -0500

    Refactor Cougar, Lion, and Tiger classes to extend Animal class, adding constructors and species methods. Clean up enclosure-view.fxml by removing invalid tags and correcting controller path.

--- Assigment-3-ZMS/src/main/java/com/example/assigment3zms/Model/Lion.java
+++ Assigment-3-ZMS/src/main/java/com/example/assigment3zms/Model/Lion.java
@@ -1,5 +1,14 @@
 package com.example.assigment3zms.Model;
+/**
+ * Represents a Lion, inheriting from the Animal base class.
+ */
+public class Lion extends Animal {
+    public Lion(String name, int age) {
+        super(name, age);
+    }
 
-public class Lion {
-    
+    @Override
+    public String getSpecies() {
+        return "Lion";
+    }
 }
