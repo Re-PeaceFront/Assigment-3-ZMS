c35d8307c967adc3091259d96329d3eb60ba1fe2
Gasore Rene Kazuba <167144827+Re-PeaceFront@users.noreply.github.com>
2025-11-06 21:25:54 -0500

    Add EnclosureCollection interface with Composite pattern

--- Assigment-3-ZMS/src/main/java/com/example/assigment3zms/Model/EnclosureCollection.java
+++ Assigment-3-ZMS/src/main/java/com/example/assigment3zms/Model/EnclosureCollection.java
@@ -1,5 +1,38 @@
 package com.example.assigment3zms.Model;
 
-public class EnclosureCollection {
-    
+import javafx.collections.ObservableList;
+
+/**
+ * Component interface for the Composite Design Pattern.
+ * Defines the common operations for both leaf (Enclosure) and
+ * composite (CompositeEnclosureCollection) nodes.
+ *
+ * @author Ren (Student)
+ * @version 1.0
+ */
+public interface EnclosureCollection {
+
+    /**
+     * Gets the name of the enclosure or collection.
+     *
+     * @return The display name.
+     */
+    String getName();
+
+    /**
+     * Gets the children of this node.
+     * For a Composite, this returns its list of sub-collections.
+     * For a Leaf (Enclosure), this returns null.
+     *
+     * @return An ObservableList of EnclosureCollection children, or null.
+     */
+    ObservableList<EnclosureCollection> getChildren();
+
+    /**
+     * Checks if this node is a composite node (i.e., can have children).
+     *
+     * @return true if it's a composite (like a Section),
+     * false if it's a leaf (like an Enclosure).
+     */
+    boolean isComposite();
 }
