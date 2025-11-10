df28a79c76cf82b503a0bba9e1fe2f730927f833
Emmanuelle <EmmanuelleIsis.Mbeug@stu.crc-lennox.qc.ca>
2025-11-10 15:52:18 -0500

    Implemented the CompositeEnclosureCollectionController

--- Assigment-3-ZMS/src/main/java/com/example/assigment3zms/Controllers/CompositeEnclosureCollectionViewController.java
+++ Assigment-3-ZMS/src/main/java/com/example/assigment3zms/Controllers/CompositeEnclosureCollectionViewController.java
@@ -1,10 +1,20 @@
 package com.example.assigment3zms.Controllers;
 
+import com.example.assigment3zms.Helpers.ImportHelper;
+import com.example.assigment3zms.Model.CompositeEnclosureCollection;
+import com.example.assigment3zms.Model.Enclosure;
 import com.example.assigment3zms.Model.EnclosureCollection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class CompositeEnclosureCollectionViewController {
@@ -23,8 +33,65 @@ public class CompositeEnclosureCollectionViewController {
     @FXML
     private Button closeButton;
 
+    private CompositeEnclosureCollection rootCollection;
+    private ObservableList<EnclosureCollection> displayedChildren;
+
+    @FXML
+    private void initialize() {
+        // Load Big Cats from ImportHelper
+        rootCollection = ImportHelper.createAnimals();
+
+        // Load the children into the ListView
+        displayedChildren = FXCollections.observableArrayList(rootCollection.getChildren());
+        enclosureListView.setItems(displayedChildren);
+
+        searchTextField.textProperty().addListener((observable, oldValue, newValue) -> filterList(newValue));
+
+    }
+
+    private void filterList(String searchTerm) {
+        if (searchTerm == null || searchTerm.isEmpty()) {
+            displayedChildren.setAll(rootCollection.getChildren());
+            return;
+        }
+
+        displayedChildren.setAll(rootCollection.getChildren().stream()
+                .filter(collection -> collection.getName().toLowerCase()
+                        .contains(searchTerm.toLowerCase()))
+                            .toList()
+        );
+    }
+
     @FXML
     private void onOpenButtonClick() {
+        EnclosureCollection selected =  enclosureListView.getSelectionModel().getSelectedItem();
+        if (selected == null) return;
+
+        if (selected.isComposite()) {
+            javafx.scene.control.Alert alert = new javafx.scene.control.Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid Selection");
+            alert.setHeaderText("Invalid Selection");
+            alert.setContentText("Please select an enclosure");
+            alert.showAndWait();
+            return;
+        }
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("enclosure-view.fxml"));
+            Parent view = loader.load();
+
+            EnclosureViewController controller = loader.getController();
+            controller.setEnclosure((Enclosure) selected);
+
+            Stage nextStage = new Stage();
+            nextStage.setScene(new Scene(view));
+            nextStage.initModality(Modality.WINDOW_MODAL);
+            nextStage.initOwner(openButton.getScene().getWindow());
+            nextStage.showAndWait();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -39,5 +106,4 @@ public class CompositeEnclosureCollectionViewController {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
     }
-
 }
--- Assigment-3-ZMS/src/main/java/com/example/assigment3zms/Model/CompositeEnclosureCollection.java
+++ Assigment-3-ZMS/src/main/java/com/example/assigment3zms/Model/CompositeEnclosureCollection.java
@@ -1,5 +1,39 @@
 package com.example.assigment3zms.Model;
 
-public class CompositeEnclosureCollection {
-    
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class CompositeEnclosureCollection implements EnclosureCollection{
+    private final String name;
+    private final ObservableList<EnclosureCollection> children;
+
+   public CompositeEnclosureCollection(String name){
+       this.name = name;
+       this.children = FXCollections.observableArrayList();
+   }
+
+   public void addCollection(EnclosureCollection collection) {
+       this.children.add(collection);
+   }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public ObservableList<EnclosureCollection> getChildren() {
+        return this.children;
+    }
+
+    @Override
+    public boolean isComposite() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+       return this.name;
+    }
+
 }
